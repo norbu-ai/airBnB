b2afe10cbfcf3b6a88fe74fefb740f31001d2292
akyeshi <akyoshicode@gmail.com>
2022-12-11 14:43:20 -0500

    signup form modal feature done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,8 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
 // import LoginFormPage from "./components/LoginFormPage";
-
-import SignupFormPage from "./components/SignupFormPage";
+// import SignupFormPage from "./components/SignupFormPage";
 import * as sessionActions from "./store/session";
 import Navigation from "./components/Navigation";
 
@@ -26,9 +25,9 @@ function App() {
             <LoginFormPage />
           </Route> */}
 
-          <Route path="/signup">
+          {/* <Route path="/signup">
             <SignupFormPage />
-          </Route>
+          </Route> */}
         </Switch>
       )}
     </>
--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -4,6 +4,7 @@ import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import OpenModalButton from '../OpenModalButton'; 
 import LoginFormModal from '../LoginFormModal'; 
+import SignupFormModal from '../SignupFormModal';
 import './Navigation.css';
 
 export default function Navigation({ isLoaded }){
@@ -31,7 +32,12 @@ export default function Navigation({ isLoaded }){
                 buttonText="Log In"
                 modalComponent={<LoginFormModal />}
             />
-            <NavLink to="/signup">Sign Up</NavLink>
+
+            <OpenModalButton
+                buttonText="Sign Up"
+                modalComponent={<SignupFormModal />}
+            />
+
         </li>
         );
     }
